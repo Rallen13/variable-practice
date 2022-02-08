f688dcf3115bb95e96365d8ec6032449d953c7ae
Rachel Allen <rallenco13@gmail.com>
2022-02-07 20:26:44 -0700

    annotated variables.js

--- variable.js
+++ variable.js
@@ -1,9 +1,12 @@
 var email = "rallenco13@gmail.com";
+//this is Rachel Allens email address
 var firstName = "Rachel";
 var lastName = "Allen";
 var age = 36
 var startingAge = 36
+//age when starting the program
 var endingAge = 37
+//age when ending the program
 var isEnrolled = true
 var isHungry = true
 var isTired = true
